05d80fc21c2f5f7fd42e7c69e01743571ccc6d8f
Kerubo0 <mmyers@kabarak.com>
2024-02-07 22:18:08 +0300

    modified index.html style.css app.js

--- app.js
+++ app.js
@@ -21,23 +21,23 @@ $(document).ready(function(){
       responsiveClass:true,
       center: true,
       nav:true,
+      dots: true,
       NavText:[
-        "<i class= 'fa fa-angle-left'></i>",
-        "<i class= 'fa fa-angle-right'></i>",
+        "<i class='fa-solid fa-angle-left'></i>",
+        "<i class='fa-solid fa-angel-right'></i>",
       ],
       responsive:{
           0:{
               items:1,
-              
+              nav:true
           },
           600:{
               items:3,
              
           },
-          //1000:{
-              //items:5,
-             
-          //}
+          1000:{
+              items:3,
+          }
       }
     })
   })
