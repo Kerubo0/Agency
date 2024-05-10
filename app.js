9561c947abfaab2ab5c1280ee8059309a7d723ef
Kerubo0 <mmyers@kabarak.com>
2024-05-10 12:18:03 +0300

    adjusted the carousel

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+/*$(document).ready(function(){
   $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
@@ -24,5 +24,37 @@ $(document).ready(function(){
         }
     }
   })
-})
+}) */
+
+// Testimonial section slider...owl carousel
+jQuery(document).ready(function ($) {
+  "use strict";
+  //  TESTIMONIALS CAROUSEL HOOK
+  $('#customers-testimonials').owlCarousel({
+    loop: true,
+    center: true,
+    items: 3,
+    margin: 0,
+    autoplay: true,
+    dots: true,
+    autoplayTimeout: 8500,
+    smartSpeed: 450,
+    responsive: {
+      0: {
+        items: 1
+      },
+      768: {
+        items: 2
+      },
+      1170: {
+        items: 3
+      }
+    }
+  });
+});
+// End testimonial section slider
+
+
+
+
 
