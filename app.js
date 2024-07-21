89e0275f398935aa12720f97e9be07b10d40f1a6
Kerubo0 <mmyers@kabarak.com>
2024-07-21 16:57:56 +0300

    added an empty readme and changed a few typos

--- app.js
+++ app.js
@@ -1,30 +1,82 @@
-/*$(document).ready(function(){
-  $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    responsiveClass:true,
-    center: true,
-    nav:true,
-    dots: true,
-    NavText:[
-      "<i class='fa-solid fa-angle-left'></i>",
-      "<i class='fa-solid fa-angel-right'></i>",
-    ],
-    responsive:{
-        0:{
-            items:1,
-            nav:true
-        },
-        600:{
-            items:3,
-           
-        },
-        1000:{
-            items:3,
-        }
-    }
-  })
-}) */
+const toggleBtn = document.querySelector('.toggle_btn')
+const toggleBtnIcon = document.querySelector('.toggle_btn i')
+const dropDownMenu = document.querySelector('.dropdown_menu')
+
+toggleBtn.onclick = function () {
+dropDownMenu.classList.toggle('open')
+const isOpen = dropDownMenu.classList.contains('open')
+
+toggleBtnIcon.classList  = isOpen 
+? 'fa-solid fa-xmark'
+: 'fa-solid fa-bars'
+}
+
+const sLink = document.querySelector(".s_link")
+
+sLink.addEventListener ("click", () => {
+  if (sLink.style.display === "none" || sLink.style.display === "") {
+    sLink.style.display = "block";
+} else {
+  //sLink.style.display = "none";
+}
+})
+
+//Join waitlist button start 
+
+const btn = document.getElementById('action_btn');
+const overlay = document.getElementById('overlay');
+
+btn.addEventListener("click", () => {
+  overlay.style.display = 'flex';
+  document.body.classList.add('no-scroll');
+});
+
+overlay.addEventListener("click", (e) => {
+  if (e.target === overlay) {
+    overlay.style.display = 'none';
+    document.body.classList.remove('no-scroll');
+  }
+});
+
+
+//Join waitlist button end
+
+const root = document.documentElement;
+
+const setDropdownProps = (button, list, deg, ht, opacity) => {
+    //button.querySelector('.dropdown-arrow').style.transform = `rotate(${deg}deg)`;
+    button.querySelector('.angle').style.transform = `rotate(${deg}deg)`;
+    list.style.maxHeight = ht;
+    list.querySelectorAll('.dropdown-list-item').forEach(item => {
+        item.style.opacity = opacity;
+    });
+};
+
+document.querySelectorAll('.dropdown-button').forEach(button => {
+    const list = button.nextElementSibling;
+    
+    const listWrapperSizes = 3.5; // margins, paddings & borders (3.5rem)
+
+    button.addEventListener('mouseenter', () => {
+        const dropdownOpenHeight = `${100 * list.children[0].children.length + listWrapperSizes}rem`;
+        setDropdownProps(button, list, 180, dropdownOpenHeight, 1);
+    });
+
+    button.addEventListener('mouseleave', () => {
+        setDropdownProps(button, list, 0, '0', 0);
+    });
+
+    list.addEventListener('mouseenter', () => {
+        const dropdownOpenHeight = `${100 * list.children[0].children.length + listWrapperSizes}rem`;
+        setDropdownProps(button, list, 180, dropdownOpenHeight, 1);
+    });
+
+    list.addEventListener('mouseleave', () => {
+        setDropdownProps(button, list, 0, '0', 0);
+    });
+});
+
+
 
 // Testimonial section slider...owl carousel
 jQuery(document).ready(function ($) {
